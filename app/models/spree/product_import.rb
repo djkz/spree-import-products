86c847151dfea8a9b4917e121f2478b0b9b03f65
Timur Kunayev <timur.kunayev@gmail.com>
2012-05-17 19:59:46 -0700

    enabled S3 for cvs upload

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -7,8 +7,20 @@
 module Spree
 	class ProductImport < ActiveRecord::Base
           attr_accessible :data_file
-	  has_attached_file :data_file, :path => ":rails_root/lib/etc/product_data/data-files/:basename.:extension"
 	  validates_attachment_presence :data_file
+
+          has_attached_file :data_file,
+              :styles => { :normal => '200x200>', :mini=>'60x60'  },
+              :url => '/spree/cvs/:id/:basename.:extension',
+	      :path => ":rails_root/lib/etc/product_data/data-files/:basename.:extension"
+
+          if Spree::Config[:use_s3]
+              s3_creds = { :access_key_id => Spree::Config[:s3_access_key], :secret_access_key => Spree::Config[:s3_secret], :bucket => Spree::Config[:s3_bucket] }
+              Spree::Brand.attachment_definitions[:data_file][:storage] = :s3
+              Spree::Brand.attachment_definitions[:data_file][:s3_credentials] = s3_creds
+              Spree::Brand.attachment_definitions[:data_file][:s3_headers] = ActiveSupport::JSON.decode(Spree::Config[:s3_headers])
+              Spree::Brand.attachment_definitions[:data_file][:bucket] = Spree::Config[:s3_bucket]
+          end
 
 	  require 'csv'
 	  require 'pp'
